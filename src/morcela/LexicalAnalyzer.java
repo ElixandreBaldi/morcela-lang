fcb56b03e8ab1ac4228db217093826c335e5c585
Luiz Guilherme Fonseca Rosa <luizguilhermefr@gmail.com>
2018-05-23 14:16:24 -0300

    It's secret!

---
+++ src/morcela/LexicalAnalyzer.java
@@ -0,0 +1,13 @@
+package morcela;
+
+import java.io.BufferedReader;
+
+class LexicalAnalyzer {
+    LexicalAnalyzer(BufferedReader bf) {
+
+    }
+
+    void run() {
+
+    }
+}
--- src/morcela/Main.java
+++ src/morcela/Main.java
@@ -1,7 +1,67 @@
 package morcela;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
 public class Main {
-    public static void main (String args[]) {
-        System.out.println("Hello world");
+    private static int ARGS_COUNT = 1;
+
+    private static int ARG_INPUT_FILE = 0;
+
+    public static void main(String args[]) {
+        try {
+            parseArguments(args);
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+
+        BufferedReader bf = null;
+        try {
+            bf = openArgumentFile(args[ARG_INPUT_FILE]);
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
+            System.exit(1);
+        }
+
+        printAsciiWelcome();
+        runLexicalAnalysis(bf);
+
+        System.exit(0);
+    }
+
+    private static void parseArguments(String args[]) throws IllegalArgumentException {
+        if (args.length != ARGS_COUNT) {
+            throw new IllegalArgumentException("Invalid arguments. Only one is expected.");
+        }
+    }
+
+    private static BufferedReader openArgumentFile(String filename) throws FileNotFoundException {
+        File file = new File(filename);
+        FileReader fr = new FileReader(file);
+        return new BufferedReader(fr);
+    }
+
+    private static void printAsciiWelcome() {
+        System.out.println("      ___           ___           ___           ___           ___           ___       ___     \n" +
+                "     /\\__\\         /\\  \\         /\\  \\         /\\  \\         /\\  \\         /\\__\\     /\\  \\    \n" +
+                "    /::|  |       /::\\  \\       /::\\  \\       /::\\  \\       /::\\  \\       /:/  /    /::\\  \\   \n" +
+                "   /:|:|  |      /:/\\:\\  \\     /:/\\:\\  \\     /:/\\:\\  \\     /:/\\:\\  \\     /:/  /    /:/\\:\\  \\  \n" +
+                "  /:/|:|__|__   /:/  \\:\\  \\   /::\\~\\:\\  \\   /:/  \\:\\  \\   /::\\~\\:\\  \\   /:/  /    /::\\~\\:\\  \\ \n" +
+                " /:/ |::::\\__\\ /:/__/ \\:\\__\\ /:/\\:\\ \\:\\__\\ /:/__/ \\:\\__\\ /:/\\:\\ \\:\\__\\ /:/__/    /:/\\:\\ \\:\\__\\\n" +
+                " \\/__/~~/:/  / \\:\\  \\ /:/  / \\/_|::\\/:/  / \\:\\  \\  \\/__/ \\:\\~\\:\\ \\/__/ \\:\\  \\    \\/__\\:\\/:/  /\n" +
+                "       /:/  /   \\:\\  /:/  /     |:|::/  /   \\:\\  \\        \\:\\ \\:\\__\\    \\:\\  \\        \\::/  / \n" +
+                "      /:/  /     \\:\\/:/  /      |:|\\/__/     \\:\\  \\        \\:\\ \\/__/     \\:\\  \\       /:/  /  \n" +
+                "     /:/  /       \\::/  /       |:|  |        \\:\\__\\        \\:\\__\\        \\:\\__\\     /:/  /   \n" +
+                "     \\/__/         \\/__/         \\|__|         \\/__/         \\/__/         \\/__/     \\/__/    ");
+        System.out.println();
+        System.out.println("Morcela Lang 0.1");
+    }
+
+    private static void runLexicalAnalysis(BufferedReader bf) {
+        LexicalAnalyzer lexicalAnalyzer = new LexicalAnalyzer(bf);
+        lexicalAnalyzer.run();
     }
 }
