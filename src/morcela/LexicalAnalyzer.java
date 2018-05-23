0ef535bb7b790084ca0f8ef8652982e7c3354a08
Luiz Guilherme Fonseca Rosa <luizguilhermefr@gmail.com>
2018-05-23 15:07:45 -0300

    ADD MORE WORDS

--- src/morcela/LexicalAnalyzer.java
+++ src/morcela/LexicalAnalyzer.java
@@ -15,8 +15,48 @@ class LexicalAnalyzer {
 
     void run() {
         for(int i = 0; i < content.length(); i++) {
-            if(content.charAt(i) == '*') {
+            char charCurrent = content.charAt(i);
+            if(charCurrent == '*') {
                 tokens.add(new Token(Token.TokenType.MULT));
+            } else if(charCurrent == ',') {
+                tokens.add(new Token(Token.TokenType.COMMA));
+            } else if(charCurrent == '/') {
+
+            } else if(charCurrent == '^') {
+                tokens.add(new Token(Token.TokenType.XOR));
+            } else if(charCurrent == '|') {
+
+            } else if(charCurrent == '{') {
+                tokens.add(new Token(Token.TokenType.OPEN_BRACES));
+            } else if(charCurrent == '}') {
+                tokens.add(new Token(Token.TokenType.CLOSE_BRACES));
+            } else if(charCurrent == '"') {
+                tokens.add(new Token(Token.TokenType.QUOTE));
+            } else if(charCurrent == '(') {
+                tokens.add(new Token(Token.TokenType.OPEN_PAR));
+            } else if(charCurrent == ')') {
+                tokens.add(new Token(Token.TokenType.CLOSE_PAR));
+            } else if(charCurrent == '[') {
+                tokens.add(new Token(Token.TokenType.OPEN_SIZE_STRING));
+            } else if(charCurrent == ']') {
+                tokens.add(new Token(Token.TokenType.CLOSE_SIZE_STRING));
+            } else if(charCurrent == ':') {
+                tokens.add(new Token(Token.TokenType.DOUBLE_DOT));
+            } else if(charCurrent == ';') {
+                tokens.add(new Token(Token.TokenType.SEMICOLON));
+            } else if(charCurrent == '=') {
+                //TODO
+                //tokens.add(new Token(Token.TokenType.EQ));
+            } else if(charCurrent == '!') {
+                //TODO
+            } else if(charCurrent == '>') {
+                //TODO
+            } else if(charCurrent == '<') {
+                //TODO
+            } else if(charCurrent == '+') {
+                //TODO
+            } else if(charCurrent == '-') {
+                //TODO
             }
         }
     }
